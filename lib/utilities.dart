1cdf30204bb11b1919005f0330ea1990cc35e20b
samuel-kings <kingssamuel1997@gmail.com>
2024-05-25 00:40:36 +0100

    fixed functions not renamed in last commit

--- lib/utilities.dart
+++ lib/utilities.dart
@@ -1,11 +1,11 @@
 import 'package:utilities/classes/dialogs.dart';
 import 'package:utilities/classes/spacing.dart';
-import 'package:utilities/classes/utilities.dart';
+import 'package:utilities/classes/functions.dart';
 import 'package:utilities/classes/widgets.dart';
 
 class Utilities {
   static Dialogs get dialogs => Dialogs();
-  static Functions get utilities => Functions();
+  static Functions get functions => Functions();
   static Widgets get widgets => Widgets();
   static Spacing get spacing => Spacing();
 }
